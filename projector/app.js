63480ab67d314ae87a6fea4db0ae93e93bfa910e
Anatolii Kochetov <kochetov_anatoly@mail.ru>
2022-10-18 21:42:03 +0200

    logic added

--- projector/app.js
+++ projector/app.js
@@ -6,21 +6,27 @@ const btConfig = config.get('bluetooth');
 const projectorHost = config.get('projectorHost');
 const stopInterval = config.get('stopInterval');
 
+var isDown = false;
+
 function stop() {
     bt.sendData("STOP");
 }
 
 function onOnline() {
+    if (isDown) return;
+
     // projector is online - roll the screen down
     bt.sendData("DOWN");
-
+    isDown = true;
     // stop rolling it down after certain timeout
     setTimeout(stop, stopInterval);
 }
 
 function onOffline() {
+    if (!isDown) return;
     // project is now off - roll the screen up
     bt.sendData("UP");
+    isDown = false;
 }
 
 bt.init(btConfig.baudRate);
