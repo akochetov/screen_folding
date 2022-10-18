de76cde160de4d111798162b64f959d5cfdb1a0e
Anatolii Kochetov <kochetov_anatoly@mail.ru>
2022-10-18 21:25:16 +0200

    fixed ping

--- projector/ping.js
+++ projector/ping.js
@@ -1,8 +1,19 @@
-module.exports.ping = function(host, onOnline, onOffline){
-    execute("ping -i 1 -W 1 -v 1 " + host, function(data) {
-        if (data && data.indexOf("bytes") == 0)
-            onOnline();
-        else
-            onOffline();
+const { exec } = require("child_process");
+
+module.exports.ping = function (host, onOnline, onOffline) {
+    exec("ping -i 1 -W 1 -v 1 " + host, (error, stdout, stderr) => {
+        if (error) {
+            console.log(`Ping error: ${error.message}`);
+        }
+        if (stderr) {
+            console.log(`Ping stderr: ${stderr}`);
+        }
+        if (stdout) {
+            console.log(`Ping stdout: ${stdout}`);
+            if (stdout.indexOf("bytes") >= 0)
+                onOnline();
+            else
+                onOffline();
+        }
     });
 };
