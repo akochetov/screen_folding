15d80f758a4b6291ff3ffb8d7316a42c2c749fa5
Anatolii Kochetov <kochetov_anatoly@mail.ru>
2022-11-03 16:46:50 +0100

    Added up and down logic to screen module

--- screen/main/main.ino
+++ screen/main/main.ino
@@ -4,16 +4,21 @@
 #error Bluetooth is not enabled! Please run `make menuconfig` to and enable it
 #endif
 
-int RELAY_PIN1 = 4;
-int RELAY_PIN2 = 14;
+int RELAY_PIN_UP = 13;
+int RELAY_PIN_DOWN = 2;
 
+String strSignal;
+String SIGNAL_UP = "UP";
+String SIGNAL_DOWN = "DOWN";
+String SIGNAL_STOP = "STOP";
 
 // create bluetooth instance
 BluetoothSerial SerialBT;
 
 void initBT() {
-  SerialBT.begin("ScreenRoller"); // Bluetooth device name
-  Serial.println("The device started, now you can pair it with bluetooth.");
+  Serial.println("Starting bluetooth...");
+  SerialBT.begin("ScreenRoller");
+  Serial.println("Bluetooth started.");
 }
 
 void setup() {
@@ -22,17 +27,51 @@ void setup() {
 
   Serial.println("Booted up. Proceeding with setup...");
 
+  pinMode(RELAY_PIN_UP, OUTPUT);
+  pinMode(RELAY_PIN_DOWN, OUTPUT);
+
   // start bluetooth
   initBT();
 
-  pinMode(RELAY_PIN1, OUTPUT);
-  pinMode(RELAY_PIN2, OUTPUT);
+  Serial.println("The device started, now you can pair it with bluetooth.");
+}
+
+void up() {
+  Serial.println("Moving up...");
+  digitalWrite(RELAY_PIN_UP, HIGH);
+  delay(100);
+  digitalWrite(RELAY_PIN_UP, LOW);
+  Serial.println("Done.");
+}
+
+void down() {
+  Serial.println("Moving down...");
+  digitalWrite(RELAY_PIN_DOWN, HIGH);
+  delay(100);
+  digitalWrite(RELAY_PIN_DOWN, LOW);
+  Serial.println("Done.");
+}
+
+void stop() {
+  Serial.println("Stopping...");
+  digitalWrite(RELAY_PIN_DOWN, HIGH);
+  digitalWrite(RELAY_PIN_UP, HIGH);
+  delay(100);
+  digitalWrite(RELAY_PIN_DOWN, LOW);
+  digitalWrite(RELAY_PIN_DOWN, LOW);
+  Serial.println("Done.");
 }
 
 void loop() {
   // print out whatever comes from bluetooth
   if (SerialBT.available()) {
-    Serial.write(SerialBT.read());
+    strSignal = SerialBT.readString();
+
+    Serial.println(strSignal);
+
+    if (strSignal == SIGNAL_UP) up();
+    if (strSignal == SIGNAL_DOWN) down();
+    if (strSignal == SIGNAL_STOP) stop();
   }
 
   delay(10);
