cfb3de54d2a98b812003c835bed3628ce215ad20
Anatolii Kochetov <kochetov_anatoly@mail.ru>
2022-11-30 15:50:10 +0100

    added lirc remotes

--- lirc/install.sh
+++ lirc/install.sh
@@ -1,3 +1,5 @@
+# https://www.instructables.com/Setup-IR-Remote-Control-Using-LIRC-for-the-Raspber/
+
 sudo apt-get install lirc
 
 cp ./lirc_options.conf /etc/lirc/lirc_options.conf
@@ -6,6 +8,9 @@ cp ./lircd.conf /etc/lirc/lircd.conf
 
 sed -i "s/#dtoverlay=gpio-ir,gpio_pin=17/dtoverlay=gpio-ir,gpio_pin=17/g" /boot/config.txt
 sed -i "s/#dtoverlay=gpio-ir-tx,gpio_pin=18/dtoverlay=gpio-ir-tx,gpio_pin=18/g" /boot/config.txt
+
+ cp projector.lircd.conf /etc/lirc/lircd.conf.d/
+ cp soundbar.lircd.conf /etc/lirc/lircd.conf.d/
 
 /etc/init.d/lircd start
 /etc/init.d/lircd status
